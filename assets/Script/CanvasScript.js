9cfb1ef109fc7228084b3ae787084aa097ee3bc1
Ishmam Mahmud <imahmud@ust.hk>
2019-03-18 23:07:41 +0800

    Implemented eliminate choice button

--- assets/Script/CanvasScript.js
+++ assets/Script/CanvasScript.js
@@ -8,11 +8,12 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
+// Reset answer entry to choice number so easier to access.
 var question = {
-    1: {btnTxts: ['香烟','啤酒','口红','包'], answer: '口红'},
-    2: {btnTxts: ['香烟2','啤酒','口红','包'], answer: '香烟2'},
-    3: {btnTxts: ['香烟3','啤酒','口红','包'], answer: '香烟3'},
-    4: {btnTxts: ['香烟4','啤酒','口红','包'], answer: '香烟4'},
+    1: {btnTxts: ['香烟','啤酒','口红','包'], answer: 2},
+    2: {btnTxts: ['香烟2','啤酒','口红','包'], answer:0},
+    3: {btnTxts: ['香烟3','啤酒','口红','包'], answer:0},
+    4: {btnTxts: ['香烟4','啤酒','口红','包'], answer:0},
 }
 
 //var questionArr = ['香烟','啤酒','口红','包'];
@@ -33,8 +34,16 @@ cc.Class({
             type: cc.Label
         },
 
+        buttonArray:
+        {
+            default : [],
+            type: cc.Button,
+        },
+
         questionId : 0,
         score : 0,
+
+        choicesLeft: 4,
 
         scoreLable : {
             default: null,
@@ -76,12 +85,19 @@ cc.Class({
         flag = true
         if(questionId > 4) return;
         this.questionId = questionId;
+        // Change the picture
         var child = this.node.getChildByName('Picture' + this.questionId);
         child.active = true;
+        // Set buttons to interactable
+        for(var i = 0; i < this.buttonArray.length; i++)
+        {
+            this.buttonArray[i].interactable = true;
+        }
         for(var i = 0; i < this.arr.length; i++)
         {
             this.arr[i].string = question[questionId].btnTxts[i];
         }
+        this.choicesLeft = 4;
     },
 
     selectAnswer(event, id){
@@ -92,7 +108,7 @@ cc.Class({
         var index;
         for(index = 0; index < question[this.questionId].btnTxts.length; index++)
         {
-            if( question[this.questionId].btnTxts[index] == question[this.questionId].answer)
+            if( index == question[this.questionId].answer)
             {
                 break;
             }
@@ -118,15 +134,47 @@ cc.Class({
 
         this.scoreLable.string = this.score;
 
+        // Set buttons to non-interactable
+        for(var i = 0; i < this.buttonArray.length; i++)
+        {
+            this.buttonArray[i].interactable = false;
+            this.choicesLeft = 0;
+        }
+
         setTimeout(function() {
             target.color = cc.color(255,255,255);
             this.setButton(this.questionId + 1);
         }.bind(this), 1000);
     },
 
+    // Function runs when the eliminate choice button is used.
+    eliminateOneChoice()
+    {
+        var choice = Math.floor(Math.random() * 4);
+        console.log(choice);
+        // Keep picking random choice if it is the answer or it has already been eliminated
+        while ((choice == question[this.questionId].answer) || (!(this.buttonArray[choice].interactable)))
+        {
+            var choice = Math.floor(Math.random() * 4);
+            console.log(choice);
+        }
+        this.buttonArray[choice].interactable = false;
+        this.choicesLeft--;
+    },
+
     playMusic(){
         this.node.getComponent('cc.AudioSource').play();
-    }
+    },
 
-    // update (dt) {},
+    update (dt)
+    {
+        // If too few choices left, disable eliminate choice button
+        if (this.choicesLeft <= 1)
+        {
+            this.buttonArray[4].interactable = false;
+        } else
+        {
+            this.buttonArray[4].interactable = true;
+        }
+    },
 });
